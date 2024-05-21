aa95b7bc47152d08d8df4c7f37e1a8b4c3a8170c
Vlady <vlad.mytia@gmail.com>
2024-05-21 14:12:05 +0200

    second task

---
+++ 2_task.js
@@ -0,0 +1,30 @@
+let array = [1,true,'hey amigo', 3, false, 8,true,12];
+
+function checkValue (array){
+    let numb= []
+    for (let i=0; i<array.length; i++){
+        if(typeof array[i]=== 'number'){
+            numb.push(array[i])
+        }
+    }
+    console.log(numb)
+
+
+    function chekNumbers(numb){
+        let number=0;
+        let count=0;
+        for(let i=0; i<numb.length; i++){
+            number+=numb[i];
+            count++
+        }
+        console.log(number / count);
+
+
+
+
+    }
+    chekNumbers(numb)
+
+}
+
+checkValue(array)
